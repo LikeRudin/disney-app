2a28a3031bcbe35ad5d3d155894637a24d2cbed1
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-09-24 22:56:37 +0900

    Update header.tsx
    give reset and random button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,34 @@
 import styled from "styled-components";
-import { ResetButton } from "./resetbutton"
+import { ResetButton } from "./resetbutton";
 import { HomeButton } from "./homebutton";
+import { RandomButton } from "./randombutton";
 
-const Nav = styled.nav`
-`
+const Nav = styled.div`
+align-self: center;
+top: 0;
+height: 30%;
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;`;
+
 
 interface HeaderProps {
-    onResetClick: () => void
+  onResetClick?: () => void;
+  onRandomClick?: () => void;
 }
 
-export const Header = ({onResetClick}: HeaderProps) => {
 
-    return (
+export const Header = ({ onResetClick, onRandomClick}: HeaderProps) => {
+  return (
     <Nav>
-        <HomeButton/>
-        <ResetButton onResetClick={onResetClick}/>    
+      <HomeButton />
+      {onResetClick ? (
+        <ResetButton onResetClick={onResetClick} />) : 
+       (
+       <RandomButton onRandomClick={onRandomClick}/>
+       )}
     </Nav>
-    )
-    
-}
+  );
+};
