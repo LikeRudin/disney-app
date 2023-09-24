acf89068d80f3d2be06339dd379798070dfc843b
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-09-24 22:05:30 +0900

    Create background.tsx
    this is for giving background to each page

---
+++ src/components/styled/background.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  background: url("/background.jpg");
+  background-size: cover;
+  z-index: -1;
+`;
