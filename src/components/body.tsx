cf466e37900edb0cdfd264d73dc08018b5ac5586
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-09-24 22:06:39 +0900

    give css
    add css to styled component

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -11,9 +11,18 @@ interface BodyProps {
   showingList: PreviewInfo[]
 }
 
-const Ul = styled.ul``
+const Ul = styled.ul`
+display:flex;
+justfy-contents: space-around;
+width: 100%;
+flex-wrap: wrap;
+`;
 
-const Li = styled.li``
+const Li = styled.li`
+width: 22%;
+padding: 10px;
+box-sizing: border-boxl
+`;
 
 export const Body = ({showingList}:BodyProps) => {
   return(<>
